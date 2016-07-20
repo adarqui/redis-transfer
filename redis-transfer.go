f26383f400cf33d3f7d76f080f68d91552464e05
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-20 17:47:52 -0400

    cleanup: splitting usage out

--- redis-transfer.go
+++ redis-transfer.go
@@ -36,37 +36,6 @@ type Op struct {
 	repch chan bool
 }
 
-func usage() {
-	fmt.Println(
-		`usage: redis-transfer <from_redis> <to_redis> <regex_or_input_file> <concurrent_threads>
-
- * There are two redis connection formats to choose from:
-   - host:port:[db[:password]]
-   - redis://user:password@host:port?db=number
-
- * regex_or_input_file:
-   - To transfer only those keys that match a regex pattern:
-     some_key_prefix*
-     *some_key_suffix
-     prefix*something*suffix
-     etc..
-
-   - To transfer keys from an input file, simply list keys in a file, separated by newlines
-     key1
-     keyN
-
- * concurrent_threads:
-   - This should be a number between 1 and (max_cpu's*10)
-     You can play around with this to find the optimal setting. I generally use 50 on my 8 core box.
-
- * examples:
-   redis-transfer localhost:6379 remotehost:6379:1:password "migrate:*" 50
-   redis-transfer redis://localhost:6379 redis://user:password@remotehost?db=1 "migrate:*" 50
-`)
-
-	os.Exit(1)
-}
-
 func parseURI(host string) (server *Redis_Server, err error) {
 	if strings.HasPrefix(host, "redis://") {
 		server, err = parseRedisURI(host)
