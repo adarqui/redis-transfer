ecdc8b5d2a93ebfe2066017643ba54aad643dcfe
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-20 17:19:35 -0400

    simple cleanup of usage output. fixing input file source

--- transfer.go
+++ transfer.go
@@ -37,7 +37,34 @@ type Op struct {
 }
 
 func usage() {
-	log.Fatal("usage: ./transfer <from_redis_host:port[:dbNum[:pass]]> <to_redis_host:port[:dbNum[:pass]]> <key-regex or input-file-full-of-keys> <number-of-concurrent-threads>")
+	fmt.Println(
+		`usage: redis-transfer <from_redis> <to_redis> <regex_or_input_file> <concurrent_threads>
+
+ * There are two redis connection formats to choose from:
+   - host:port:[db[:password]]
+   - redis://user:password@host:port?db=number
+
+ * regex_or_input_file:
+   - To transfer only those keys that match a regex pattern:
+     some_key_prefix*
+     *some_key_suffix
+     prefix*something*suffix
+     etc..
+
+   - To transfer keys from an input file, simply list keys in a file, separated by newlines
+     key1
+     keyN
+
+ * concurrent_threads:
+   - This should be a number between 1 and (max_cpu's*10)
+     You can play around with this to find the optimal setting. I generally use 50 on my 8 core box.
+
+ * examples:
+   redis-transfer localhost:6379 remotehost:6379:1:password "migrate:*" 50
+   redis-transfer redis://localhost:6379 redis://user:password@remotehost?db=1 "migrate:*" 50
+`)
+
+	os.Exit(1)
 }
 
 func parseURI(host string) (server *Redis_Server, err error) {
@@ -197,10 +224,13 @@ func (pipe *Redis_Pipe) KeysFile() chan redisKey {
 		log.Fatal("KeysFile: error reading keys file: ", err)
 	}
 	keyChan := make(chan redisKey)
-	lines := strings.Split(string(blob), "\n")
-	for _, line := range lines {
-		keyChan <- redisKey(line)
-	}
+	lines := filter(strings.Split(string(blob), "\n"), func(s string) bool { return len(s) > 0 })
+	totalKeyCount <- len(lines)
+	go func() {
+		for _, line := range lines {
+			keyChan <- redisKey(line)
+		}
+	}()
 	return keyChan
 }
 
@@ -251,7 +281,7 @@ func (pipe *Redis_Pipe) KeysRedis() chan redisKey {
 			// Preferable because it doesn't lock complete database on larger keysets for 250ms+.
 			keys, cursor, err = pipe.from.client.Scan(cursor, pipe.keys, 1000).Result()
 			if err != nil {
-				log.Fatal("KeysRedis: error obtaining keys list from redis: ", err)
+				log.Fatal("SCAN: error obtaining key scan from redis: ", err)
 			}
 			split <- keys
 
@@ -267,6 +297,7 @@ func (pipe *Redis_Pipe) KeysRedis() chan redisKey {
 }
 
 func (pipe *Redis_Pipe) Keys() chan redisKey {
+
 	_, err := os.Stat(pipe.keys)
 
 	var keys chan redisKey
@@ -274,7 +305,6 @@ func (pipe *Redis_Pipe) Keys() chan redisKey {
 		keys = pipe.KeysFile()
 	} else {
 		keys = pipe.KeysRedis()
-
 	}
 
 	return keys
@@ -303,7 +333,7 @@ func main() {
 	pipe := New(from, to, keys, threads)
 	pipes, ch := pipe.Init()
 
-	// Could be scan instead of blocking action w/ Keys
+	// Provides us with a channel that returns keys from redis or from a file
 	keyChan := pipes[0].Keys()
 
 	count := len(keyChan)
